f83c9fb43c1c78786040b82baf062ad8b04646cb
Roadsingh <roadsingh@preciousinfosystem.com>
2024-01-29 23:09:45 +0530

    end missing

--- lib/roadsingh.rb
+++ lib/roadsingh.rb
@@ -6,11 +6,15 @@ module Roadsingh
  def hello
   puts "Hello World!"
  end
+
  def printname
   puts "this is roadsingh first gem"
  end
+
  def testing(obj)
   obj.each do |o|
     puts "object are #{o}"
   end
+end
+
 end
