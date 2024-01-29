8a51d49d1f42768752409bec068a684b592534e8
Roadsingh <roadsingh@preciousinfosystem.com>
2024-01-29 22:30:21 +0530

    Init commit

---
+++ lib/roadsingh.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "roadsingh/version"
+
+module Roadsingh
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/roadsingh/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Roadsingh
+  VERSION = "0.1.0"
+end
